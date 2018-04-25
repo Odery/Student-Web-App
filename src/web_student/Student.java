d887c39b2f10be40872385daf1915eec09ab1a12
Vakamisu <kammurimini@gmail.com>
2018-04-25 22:07:31 +0300

    + actual code

--- src/web_student/Student.java
+++ src/web_student/Student.java
@@ -1,4 +1,66 @@
 package web_student;
 
 public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    Student() {
+    }
+
+    public Student(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- src/web_student/StudentDataUtil.java
+++ src/web_student/StudentDataUtil.java
@@ -1,4 +1,62 @@
 package web_student;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentDataUtil {
+    private DataSource dataSource;
+
+    public StudentDataUtil(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM student");
+
+            while (resultSet.next()) {
+                Student student = new Student();
+                student.setId(resultSet.getInt("id"));
+                student.setFirstName(resultSet.getString("first_name"));
+                student.setLastName(resultSet.getString("last_name"));
+                student.setEmail(resultSet.getString("email"));
+
+                students.add(student);
+            }
+        } catch (SQLException exc) {
+            exc.getErrorCode();
+        } finally {
+            close(connection, statement, resultSet);
+        }
+        return students;
+    }
+
+    private void close(Connection connection, Statement statement, ResultSet resultSet) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        } catch (SQLException exc) {
+            exc.getErrorCode();
+        }
+    }
 }
